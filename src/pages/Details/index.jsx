24fd0d126b2365443c1925a646e0126a95574a73
JulianaDu <julianapoisk@gmail.com>
2023-09-23 18:05:34 -0300

    first commit st09-frontend-components

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,17 @@
+ import { Fragment } from "react"
+ import { Container } from "./styles"
+
+ import { Button } from "../../components/Button"
+ 
+ /* a função deve ter o mesmo nome do arquivo; todo componente deve ter o nome inicado com letra Maiúscula*/
+ export function Details() {
+  return(
+    <Container>
+      <h1>new Hello world</h1>
+      <span>Teste</span>
+
+      <Button />
+    </Container>
+  )
+ }
+ 
