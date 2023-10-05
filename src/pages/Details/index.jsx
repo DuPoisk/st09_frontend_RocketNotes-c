3d0a1355b66629caf8b9db6697ca09ee9e4aa545
Ju Du <julianapoisk@gmail.com>
2023-10-04 23:16:08 -0300

    st09-frontend-finishing_folder_Details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,5 @@
  //import { Fragment } from "react";
- import { Container, Links } from "./styles"
+ import { Container, Links, Content } from "./styles"
 
  import { Tag } from "../../components/Tag";
  import { Button } from "../../components/Button";
@@ -12,24 +12,43 @@
   return(
     <Container>
       <Header/>
-      <h1>new Hello world</h1>
-      <span>Teste</span>
+      
 
-      <ButtonText title="Excluir nota"/>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota"/>
 
-      <Section title="Links úteis"> 
-        <Links>
-          <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
-          <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
-        </Links>
-      </Section>
+          <h1>
+            Introdução ao React
+          </h1>
 
-      <Section title="Marcadores"> 
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
+          <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
+          been the industry's standard dummy text ever since the 1500s, when an unknown printer took 
+          a galley of type and scrambled it to make a type specimen book. It has survived not only 
+          five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+           It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
+           passages, and more recently with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.
 
-      <Button title= "Voltar"/>      
+          </p>
+
+          <Section title="Links úteis"> 
+            <Links>
+              <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+              <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores"> 
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title= "Voltar"/>      
+        
+        </Content>
+      </main>
     </Container>
   ); /*<Button title="Login" loading/>     mesma coisa que escrever loading={true}*/
     /* title é a propriedade comum. O childrem é o que estiver dentro do section. children não é passado como uma propriedade comum.*/
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -8,7 +8,13 @@ export const Container = styled.div`
   grid-template-rows:105px auto; /* uma linha para o cabeçalho e outra para o conteúdo*/  
   grid-template-areas:
   "header"
-  "content"
+  "content";
+
+  > main {
+    grid-area: content;  /*uso o main para aplicar a região do grid nela*/
+    overflow-y: scroll; /* quando conteudo nao couber mais na vertical, vai aparecer uma barra de scroll*/
+    padding: 64px 0;
+  }
 `;
 
 export const Links = styled.ul`
@@ -21,4 +27,31 @@ export const Links = styled.ul`
       color: ${({theme }) => theme.COLORS.WHITE};
     }
   }
-`
+`;
+
+export const Content = styled.div`
+  max-width: 550px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+
+  > button: first-child {
+    align-self: end;
+  }
+
+  >h1 {
+    font-size: 36px;
+    font-weight: 500;
+    padding-top: 64px;
+  }
+
+  > p {
+    font-size: 16px;
+    margin-top: 16px;
+    text-align:justify;
+  }
+
+
+
+`;
