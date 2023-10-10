7ca7391d2ae21d007da137a3acbff9145511efbf
Ju Du <julianapoisk@gmail.com>
2023-10-10 00:20:01 -0300

    st09-frontend-Profile_com_avatar

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,10 @@
 
-import {FiArrowLeft, FiUser, FiMail, FiLock} from "react-icons/fi";
+import {FiArrowLeft, FiUser, FiMail, FiLock, FiCamera} from "react-icons/fi";
 
-import { Container, Form } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
+import { Container, Form, Avatar } from "./styles";
 
 export function Profile(){
   return(
@@ -13,10 +13,19 @@ export function Profile(){
         <a href="/">
           <FiArrowLeft/>
         </a>
-
       </header>
 
       <Form>
+        <Avatar>
+          <img src="https://github.com/DuPoisk.png" alt="Foto do usuário" />
+          <label htmlFor="avatar">
+            <FiCamera/>
+            <input /*  não usarei componente input, pois esse input vai ficar invisivel, so vai ser vir pra eu abrir a janela de carregar a imagem.*/
+              id="avatar"
+              type="file"
+            />
+          </label>
+        </Avatar>
         <Input
           placeholder="Nome"
           type="text"
