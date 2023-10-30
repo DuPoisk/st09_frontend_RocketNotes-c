7a63ab9c1aac8815eef9706544786bb0cfe9d5de
Ju DuPoisk <julianapoisk@gmail.com>
2023-10-29 22:40:24 -0300

    st10-3.6-Criando contexto

---
+++ src/myContext.js
@@ -0,0 +1,3 @@
+import {createContext} from "react";
+
+export const MyContext = createContext({}); // dentro de {} vai um valor default. mas com nao tenho nada para deixar como default, ele permanece vazio, apenas
